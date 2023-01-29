c3bdf8be7c3b04144b1bb2b70d87022b9a631093
Dhaval Patel <dhavalp.dev@gmail.com>
2023-01-29 11:21:03 +0530

    Favicon added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,13 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
         <link href="https://fonts.googleapis.com/css2?family=Lato&family=Roboto&display=swap" rel="stylesheet" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body className="relative bg-neutral-50 font-roboto text-neutral-700 transition-colors duration-150 dark:bg-neutral-900 dark:text-neutral-200">
         <Main />
